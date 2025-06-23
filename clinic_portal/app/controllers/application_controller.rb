9a67bb3912bef6055ae146d84a34c20fe4883e45
Anjaneyulu Chinni <165560264+AnjaneyuluChinni@users.noreply.github.com>
2025-06-23 18:32:07 +0530

    Update application_controller.rb

--- clinic_portal/app/controllers/application_controller.rb
+++ clinic_portal/app/controllers/application_controller.rb
@@ -3,11 +3,20 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def receptionist_only
     unless current_user&.role == "receptionist"
       redirect_to patients_path, alert: "Access denied."
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
   end
 end
